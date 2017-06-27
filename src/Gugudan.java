4a70b1bae2314c7b83d857e98a210b24d0f3e1e6
LHS <LHS@DESKTOP-A48G9NV>
2017-06-27 18:04:09 +0900

    배열로 구구단 다시 구현

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -3,21 +3,48 @@ import java.util.Scanner;
 public class Gugudan {
 	public static void main(String[] args){
 
-		//8, 9단
-		System.out.println("구구단 중 출력할 단은? : ");
-		Scanner scanner = new Scanner(System.in);
-		int number = scanner.nextInt();
-		System.out.println("사용자가 입력한 값 : " + number);
-		
-		if(number < 2){
-			System.out.println("값을 잘못 입력하셨습니다.");
-		} else if(number > 9){
-			System.out.println("값을 잘못 입력하셨습니다.");
-		} else{
-			for(int i =1; i<10; i++){
-				System.out.println(number*i);
-			}
+		int [] result = new int[9];
+		for(int i=0; i<result.length; i++){
+			result[i] = 2*(i+1);
 		}
+		
+		for(int i=0; i<result.length; i++){
+			System.out.println(result[i]);
+		}
+		
+		int [] times3 = new int[9];
+		for(int i=0; i<times3.length; i++){
+			times3[i] = 3*(i+1);
+		}
+		
+		for(int i=0; i<times3.length; i++){
+			System.out.println(times3[i]);
+		}
+
+		int [] times4 = new int[9];
+		for(int i=0; i<times4.length; i++){
+			times4[i] = 3*(i+1);
+		}
+		
+		for(int i=0; i<times4.length; i++){
+			System.out.println(times4[i]);
+		}
+
+		//8, 9단
+//		System.out.println("구구단 중 출력할 단은? : ");
+//		Scanner scanner = new Scanner(System.in);
+//		int number = scanner.nextInt();
+//		System.out.println("사용자가 입력한 값 : " + number);
+//		
+//		if(number < 2){
+//			System.out.println("값을 잘못 입력하셨습니다.");
+//		} else if(number > 9){
+//			System.out.println("값을 잘못 입력하셨습니다.");
+//		} else{
+//			for(int i =1; i<10; i++){
+//				System.out.println(number*i);
+//			}
+//		}
 		
 		//2단
 		
