a38a2382dddac54d07eba78fb162b169f7e70040
LHS <LHS@DESKTOP-A48G9NV>
2017-06-27 18:07:16 +0900

    2중 for문을 이용한 구구단 구현

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -3,31 +3,18 @@ import java.util.Scanner;
 public class Gugudan {
 	public static void main(String[] args){
 
+		int dan;
 		int [] result = new int[9];
-		for(int i=0; i<result.length; i++){
-			result[i] = 2*(i+1);
-		}
-		
-		for(int i=0; i<result.length; i++){
-			System.out.println(result[i]);
-		}
-		
-		int [] times3 = new int[9];
-		for(int i=0; i<times3.length; i++){
-			times3[i] = 3*(i+1);
-		}
-		
-		for(int i=0; i<times3.length; i++){
-			System.out.println(times3[i]);
-		}
-
-		int [] times4 = new int[9];
-		for(int i=0; i<times4.length; i++){
-			times4[i] = 3*(i+1);
-		}
-		
-		for(int i=0; i<times4.length; i++){
-			System.out.println(times4[i]);
+		for(dan=2; dan<10; dan++)
+		{
+			System.out.println(dan + "´Ü");
+			for(int i=0; i<result.length; i++){
+				result[i] = dan*(i+1);
+			}
+			
+			for(int i=0; i<result.length; i++){
+				System.out.println(result[i]);
+			}
 		}
 
 		//8, 9´Ü
