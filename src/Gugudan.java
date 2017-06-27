1dcfe9317d72ba730326659573e495842210c558
LHS <LHS@DESKTOP-A48G9NV>
2017-06-27 17:44:25 +0900

    6단&7단 while,for문 이용

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -46,20 +46,32 @@ public class Gugudan {
 //		result = 4 * 9;
 //		System.out.println(result);
 
-		System.out.println("구구단 중 출력할 단은? : ");
-		Scanner scanner = new Scanner(System.in);
-		int number = scanner.nextInt();
-		
-		System.out.println(number *1);
-		System.out.println(number *2);
-		System.out.println(number *3);
-		System.out.println(number *4);
-		System.out.println(number *5);
-		System.out.println(number *6);
-		System.out.println(number *7);
-		System.out.println(number *8);
-		System.out.println(number *9);
+//		System.out.println("구구단 중 출력할 단은? : ");
+//		Scanner scanner = new Scanner(System.in);
+//		int number = scanner.nextInt();
+//		
+//		System.out.println(number *1);
+//		System.out.println(number *2);
+//		System.out.println(number *3);
+//		System.out.println(number *4);
+//		System.out.println(number *5);
+//		System.out.println(number *6);
+//		System.out.println(number *7);
+//		System.out.println(number *8);
+//		System.out.println(number *9);
 
+		//6단
+		int i = 1;
+		while(i < 10){
+			System.out.println(i*6);
+			i = i + 1;
+		}
+		
+		//7단
+		for(int j = 1; j < 10; j++){
+			System.out.println(j*7);
+		}
+		
 
 	}
 }
